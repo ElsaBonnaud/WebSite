b07aaf13bb2a7567ede8e22763478bd2d2640df4
Eric Hartmann <112486+ehartmann@users.noreply.github.com>
2022-11-20 18:56:26 +0100

    Better up.sh

--- up.sh
+++ up.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
 git pull
-hugo --theme=elsa -D
-java -jar bin/htmlcompressor-1.5.3.jar -r public/ -o public/
-find public \( -name "*.css" -o -name "*.js" \) -exec java -jar bin/yuicompressor-2.4.8.jar {}  -o {} \;
+hugo --minify
+#java -jar bin/htmlcompressor-1.5.3.jar -r public/ -o public/
+#find public \( -name "*.css" -o -name "*.js" \) -exec java -jar bin/yuicompressor-2.4.8.jar {}  -o {} \;
